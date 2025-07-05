ecd69250b8192bf99f882bcdd431cd085db3d165
praneeth <gp.gunturu@gmail.com>
2025-07-05 11:57:38 -0500

    Managing Stack and BottomTabs in the App

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -2,6 +2,6 @@ import { Text } from "react-native";
 
 export default function AllExpenses() {
     return (
-        <Text>AllExpenses</Text>
+        <Text>AllExpensessdvasdvasdvsdvsd</Text>
     )
 }
