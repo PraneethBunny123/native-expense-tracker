43a763b5ea8263a313581d0af43eeab64d6c7e7a
praneeth <gp.gunturu@gmail.com>
2025-07-06 13:00:37 -0500

    Conditionallysetting title of the manage-expense by using the expenseId

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,7 +1,12 @@
 import { Text } from "react-native";
 
-export default function ManageExpenses({route}) {
-    const expenseId = route.params.expenseId
+export default function ManageExpenses({route, navigation}) {
+    const expenseId = route.params?.expenseId
+    const isEditing = !!expenseId
+
+    navigation.setOptions({
+        title: isEditing ? 'Edit Expense' : 'Add Expense'
+    })
 
     return (
         <Text>ManageExpenses</Text>
