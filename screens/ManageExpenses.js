2c1d94993b6b839be2f699a5afe8c9a05a634114
praneeth <gp.gunturu@gmail.com>
2025-07-06 13:02:35 -0500

    Wrapping the navigation.setOptions method with useLayoutEffect() hook as setting options dorectly inside the component itself is bad practice

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,12 +1,16 @@
+import { useLayoutEffect } from "react";
 import { Text } from "react-native";
 
 export default function ManageExpenses({route, navigation}) {
     const expenseId = route.params?.expenseId
     const isEditing = !!expenseId
 
-    navigation.setOptions({
-        title: isEditing ? 'Edit Expense' : 'Add Expense'
-    })
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: isEditing ? 'Edit Expense' : 'Add Expense'
+        })
+    }, [navigation, isEditing])
+    
 
     return (
         <Text>ManageExpenses</Text>
