ab889c8d1218944665f923af83f8170a0aa8c13b
praneeth <gp.gunturu@gmail.com>
2025-07-07 18:40:12 -0500

    creating a new utility function to get a date - no. of days. which means we can access expenses within that return date

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -6,6 +6,10 @@ import { ExpensesContext } from "../store/expenses-context";
 export default function RecentExpenses() {
     const expensesCtx = useContext(ExpensesContext)
 
+    const recentExpenses = expensesCtx.expenses.filter(expense => {
+        const today = new Date()
+    })
+
     return (
         <ExpensesOutput expenses={expensesCtx.expenses} period='last 7 days' />
     )
