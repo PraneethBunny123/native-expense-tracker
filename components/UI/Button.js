61984a5511ad506895a7a383245ad373c994d41a
praneeth <gp.gunturu@gmail.com>
2025-07-07 16:16:59 -0500

    adding the new styles to the tags

--- components/UI/Button.js
+++ components/UI/Button.js
@@ -4,9 +4,12 @@ import { GlobalStyles } from "../../constants/styles";
 export default function Button({children, onPress, mode}) {
     return (
         <View>
-            <Pressable onPress={onPress}>
-                <View>
-                    <Text>{children}</Text>
+            <Pressable 
+                onPress={onPress}
+                style={({pressed}) => pressed && styles.pressed}   
+            >
+                <View style={[styles.button, mode === 'flat' && styles.flat]}>
+                    <Text style={[styles.buttonText, mode === 'flat' && styles.flatText]}>{children}</Text>
                 </View>
             </Pressable>
         </View>
