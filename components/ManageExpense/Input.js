7737b6d39d83309acf244c1bfd7c1d785c82edae
praneeth <gp.gunturu@gmail.com>
2025-07-08 13:49:51 -0500

    adding cascading styles for description

--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -2,17 +2,23 @@ import { StyleSheet, Text, TextInput, View } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
 
 export default function Input({label, textInputConfig}) {
+    let inputStyles = [styles.input]
+
+    if(textInputConfig && textInputConfig.multiline) {
+        inputStyles = [...inputStyles, styles.inputMultiline]
+    }
+
     return (
         <View style={styles.inputContainer}>
             <Text style={styles.label}>{label}</Text>
-            <TextInput style={styles.input} {...textInputConfig}/>
+            <TextInput style={inputStyles} {...textInputConfig}/>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     inputContainer: {
-        marginVertical: 16,
+        marginVertical: 8,
         marginHorizontal: 4
     },
     label: {
@@ -26,5 +32,9 @@ const styles = StyleSheet.create({
         padding: 6,
         borderRadius: 6,
         fontSize: 18
+    },
+    inputMultiline: {
+        minHeight: 100,
+        textAlignVertical: 'top'
     }
 })
