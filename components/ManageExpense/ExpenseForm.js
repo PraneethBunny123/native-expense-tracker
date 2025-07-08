bcb828415d691c6adac782b830124bf81b8660bb
praneeth <gp.gunturu@gmail.com>
2025-07-08 13:37:22 -0500

    Added ExpenseForm outline using the custom Input

---
+++ components/ManageExpense/ExpenseForm.js
@@ -0,0 +1,35 @@
+import { View } from "react-native";
+import Input from "./Input";
+
+export default function ExpenseForm() {
+
+    function handleAmountChange() {
+
+    }
+
+    return (
+        <View>
+            <Input 
+                label='Amount'
+                textInputConfig={{
+                    keyboardType: 'decimal-pad',
+                    onchangeText: handleAmountChange()
+                }}
+            />
+            <Input 
+                label='Date'
+                textInputConfig={{
+                    placeholder: 'YYYY-MM-DD',
+                    maxLength: 10,
+                    onchange: () => {}
+                }}
+            />
+            <Input 
+                label='Description'
+                textInputConfig={{
+                    
+                }}
+            />
+        </View>
+    )
+}
--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -4,6 +4,7 @@ import IconButton from "../components/UI/IconButton";
 import {GlobalStyles} from '../constants/styles'
 import Button from "../components/UI/Button";
 import { ExpensesContext } from "../store/expenses-context";
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 
 export default function ManageExpenses({route, navigation}) {
     const expenseId = route.params?.expenseId
@@ -45,6 +46,7 @@ export default function ManageExpenses({route, navigation}) {
 
     return (
         <View style={styles.container}>
+            <ExpenseForm />
             <View style={styles.buttons}>
                 <Button style={styles.button} mode='flat' onPress={handleCancelButton}>Cancel</Button>
                 <Button style={styles.button} onPress={handleConfirmButton}>{isEditing ? 'Update' : 'Add'}</Button>
