f4cef74b7ec2e858c4e1796876f96131528a60f3
praneeth <gp.gunturu@gmail.com>
2025-07-08 13:44:44 -0500

    Styled the input component

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -27,7 +27,7 @@ export default function ExpenseForm() {
             <Input 
                 label='Description'
                 textInputConfig={{
-                    
+                    multiline: true
                 }}
             />
         </View>
--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -1,10 +1,30 @@
-import { Text, TextInput, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
 export default function Input({label, textInputConfig}) {
     return (
-        <View>
-            <Text>{label}</Text>
-            <TextInput {...textInputConfig}/>
+        <View style={styles.inputContainer}>
+            <Text style={styles.label}>{label}</Text>
+            <TextInput style={styles.input} {...textInputConfig}/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginVertical: 16,
+        marginHorizontal: 4
+    },
+    label: {
+        fontSize: 12,
+        color: GlobalStyles.colors.primary100,
+        marginBottom: 4
+    },
+    input: {
+        backgroundColor: GlobalStyles.colors.primary100,
+        color: GlobalStyles.colors.primary700,
+        padding: 6,
+        borderRadius: 6,
+        fontSize: 18
+    }
+})
