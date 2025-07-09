9a75860bd7f4bd08048b6f7f3750172b413b7f24
praneeth <gp.gunturu@gmail.com>
2025-07-09 17:23:29 -0500

    making the expense that is added latest to the top

--- store/expenses-context.js
+++ store/expenses-context.js
@@ -14,7 +14,8 @@ function expenseReducer(state, action) {
             const id = new Date().toString() + Math.random().toString()
             return [{...action.payload, id: id}, ...state]
         case 'SET': 
-            return action.payload
+            const inverted = action.payload.reverse()
+            return inverted
         case 'UPDATE':
             const updatableExpenseIndex = state.findIndex(expense => expense.id === action.payload.id)
             const updatableExpense = state[updatableExpenseIndex]
