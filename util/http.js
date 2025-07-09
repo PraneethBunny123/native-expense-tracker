b95de263345b67d24393310a61d82f684a144fd1
praneeth <gp.gunturu@gmail.com>
2025-07-09 18:43:47 -0500

    created update and delete methods that will do the corresponding work in the firebase

--- util/http.js
+++ util/http.js
@@ -26,3 +26,11 @@ export async function getExpenses() {
 
     return expenses
 }
+
+export function updatedExpense(id, expenseData) {
+    return axios.put(BACKEND_URL + `expenses/${id}.json`, expenseData)
+}
+
+export function deleteExpense(id) {
+    return axios.delete(BACKEND_URL + `expenses/${id}.json`)
+}
